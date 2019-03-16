848543dee0f2c8cac375a31218ccaa55cf9d7641
JALVITO <lizvizjuan@gmail.com>
2019-03-16 17:48:45 -0600

    First Commit

---
+++ Assets/Scripts/Footsteps.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Footsteps : MonoBehaviour
+{	
+
+	private Light footLight;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        footLight = GetComponent<Light>();
+        footLight.range = 10;
+        footLight.intensity = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public IEnumerator drawSound(int i, int r){
+
+    	footLight.intensity = i;
+    	footLight.range = r;
+
+    	while (footLight.intensity > 0){
+    		footLight.intensity -= i/60.0F;
+    		footLight.range += r/60.0F;
+    		yield return new WaitForSeconds(0.02F);
+    	}
+
+    }
+}
---
+++ Assets/Scripts/Shoot.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shoot : MonoBehaviour
+{
+
+	private GameObject bullet;
+	private GameObject shotSpawn;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        bullet.AddComponent<Bullet>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+            GameObject bullet = Instantiate(bullet, shotSpawn.Transform.position);
+    }
+}
