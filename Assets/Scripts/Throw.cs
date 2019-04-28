c999d8c623b341482d08bf2782e38f6319ae9439
Jose Guillermo Saldana <memo.sal98@gmail.com>
2019-04-27 19:07:51 -0500

    Developing throw for items and guns

---
+++ Assets/Scripts/Throw.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Throw : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Stack items = new Stack();
+    [SerializeField] private GameObject item;
+	  [SerializeField] private Transform shotSpawn;
+    void Start()
+    {
+      items.Push("A");
+      items.Push("A");
+      items.Push("A");
+      items.Push("A");
+      items.Push("A");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(1) && items.Count >0 ) {
+          items.Pop();
+          Debug.Log("THROWN");
+          Instantiate(item, shotSpawn.position,  shotSpawn.rotation);
+        }
+    }
+}
---
+++ Assets/Scripts/Throwable.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Throwable : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField] private int throwSpeed;
+    private Rigidbody rb;
+    
+    void Start()
+    {
+        rb = this.GetComponent<Rigidbody>();
+        rb.AddForce(transform.forward * throwSpeed);
+    }
+
+    void Update(){
+      Debug.Log(rb.velocity);
+    }
+
+}
