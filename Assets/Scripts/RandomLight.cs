ca7f1bf4edcceb16dab289689c6f7aad7608dca4
JALVITO <lizvizjuan@gmail.com>
2019-05-04 19:01:26 -0500

    Remakes radio

---
+++ Assets/Scripts/RandomLight.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomLight : MonoBehaviour
+{
+	private Light light;
+	[SerializeField] private float intensity;
+    // Start is called before the first frame update
+    void Start()
+    {
+        light = gameObject.GetComponent<Light>();
+        light.intensity = intensity;
+        StartCoroutine(randomLight());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       if(light.range>0){
+       	light.range-=0.4F;
+       }
+    }
+    private IEnumerator randomLight(){
+    	while(true){
+    		float newRange = Random.Range(0.0F,25.0F);
+    		Debug.Log(newRange);
+    		light.range = Mathf.Max(light.range, newRange);
+    		yield return new WaitForSeconds(0.04F);
+    	}
+    }
+}
